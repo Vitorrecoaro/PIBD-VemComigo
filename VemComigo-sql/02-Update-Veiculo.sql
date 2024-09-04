f317161a9560fdb123c00b4206addc2cebd396db
Vitor Recoaro <93007912+Vitorrecoaro@users.noreply.github.com>
2024-09-04 15:49:08 -0300

    fix: changing name of column to drop on update veiculo

--- VemComigo-sql/02-Update-Veiculo.sql
+++ VemComigo-sql/02-Update-Veiculo.sql
@@ -1,8 +1,8 @@
- -- Update Veiculo table to remove tracking about who is the owner
- -- and relation of the user with the owner.
+-- Update Veiculo table to remove tracking about who is the owner
+-- and relation of the user with the owner.
 ALTER TABLE
   veiculo
 DROP COLUMN IF EXISTS
   proprietario,
 DROP COLUMN IF EXISTS
-  relacaoproprietario;
+  relacaocomproprietario;

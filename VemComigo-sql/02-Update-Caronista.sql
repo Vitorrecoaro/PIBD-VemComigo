1e05c0abe8fe78aabb5cdf1b890aa970e3e24342
PedroViniciusVicente <107448622+PedroViniciusVicente@users.noreply.github.com>
2024-09-06 12:06:06 -0300

    Pedro artefatos branch (#1)
    * feat: starting my artifacts
    
    * feat: finished my artifacts
    
    * refactor: changing names and updating files to better organization
    
    * style: formating update caronista file
    
    ---------
    
    Co-authored-by: vitorrecoaro <vitordearec@gmail.com>

--- VemComigo-sql/02-Update-Caronista.sql
+++ VemComigo-sql/02-Update-Caronista.sql
@@ -1,2 +1,5 @@
--- This script update Caronista table.
-ALTER TABLE caronista ADD COLUMN IF NOT EXISTS     ValidadeCNH VARCHAR(24) NOT NULL;
+ -- This script update Caronista table.
+ALTER TABLE
+  caronista
+ADD COLUMN IF NOT EXISTS
+  ValidadeCNH VARCHAR(24) NOT NULL;

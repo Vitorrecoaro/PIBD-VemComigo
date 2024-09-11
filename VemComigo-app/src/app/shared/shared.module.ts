157fbb31642b282ff8e60e75592e5398018b62c9
vitorrecoaro <vitordearec@gmail.com>
2024-09-11 16:21:37 -0300

    feat: adding type of input

--- VemComigo-app/src/app/shared/shared.module.ts
+++ VemComigo-app/src/app/shared/shared.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputTextComponent } from '../components/generic/input-text/input-text.component';
 import { IonicModule } from '@ionic/angular';
+import { ButtonComponent } from '../components/generic/button/button.component';
 
 @NgModule({
-  declarations: [InputTextComponent],
-  imports: [
-    CommonModule,
-    IonicModule
-  ],
-  exports: [InputTextComponent]
+  declarations: [InputTextComponent, ButtonComponent],
+  imports: [CommonModule, IonicModule],
+  exports: [InputTextComponent, ButtonComponent],
 })
-export class SharedModule { }
+export class SharedModule {}

91b5068581de5ece6f486886497c0ea31995ab72
vitorrecoaro <vitordearec@gmail.com>
2024-09-11 16:20:18 -0300

    feat: adding generic button

---
+++ VemComigo-app/src/app/components/generic/button/button.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent {
+  @Input() text: string = 'button';
+  @Input() color: string = 'primary';
+
+  constructor() {}
+}

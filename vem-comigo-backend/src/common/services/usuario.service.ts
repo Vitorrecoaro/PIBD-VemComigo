392a04a71cdda0693c5d793c9b0497a3f4163b79
alrettondini <andre.retton21@gmail.com>
2024-09-14 10:46:24 -0300

    add: user CRUD (erro com ID no controller)

---
+++ vem-comigo-backend/src/common/services/usuario.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Usuario } from '../../entity/usuario.entity';
+
+@Injectable()
+export class UsuarioService {
+  constructor(
+    @InjectRepository(Usuario)
+    private userRepository: Repository<Usuario>,
+  ) {}
+
+  findAll(): Promise<Usuario[]> {
+    return this.userRepository.find();
+  }
+
+  findOne(id: 'identity'): Promise<Usuario> {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  create(user: Usuario): Promise<Usuario> {
+    return this.userRepository.save(user);
+  }
+
+  async update(id: 'identity', user: Partial<Usuario>): Promise<Usuario> {
+    await this.userRepository.update(id, user);
+    return this.userRepository.findOneBy({ id });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.userRepository.delete(id);
+  }
+}

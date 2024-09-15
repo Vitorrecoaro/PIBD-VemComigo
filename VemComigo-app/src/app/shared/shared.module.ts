7dfe16ad0f3e7ba9c7e17656644429198f418a03
vitorrecoaro <vitordearec@gmail.com>
2024-09-15 14:26:07 -0300

    feat: create user card component

--- VemComigo-app/src/app/shared/shared.module.ts
+++ VemComigo-app/src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { IonicModule } from '@ionic/angular';
 import { ButtonComponent } from '../components/generic/button/button.component';
 import { UserResumedDetailsComponent } from '../components/user-resumed-details/user-resumed-details.component';
 import { CarResumedDetailsComponent } from '../components/car-resumed-details/car-resumed-details.component';
+import { UserCardComponent } from '../components/user-card/user-card.component';
 
 @NgModule({
-  declarations: [InputTextComponent, ButtonComponent, UserResumedDetailsComponent, CarResumedDetailsComponent],
+  declarations: [InputTextComponent, ButtonComponent, UserResumedDetailsComponent, CarResumedDetailsComponent, UserCardComponent],
   imports: [CommonModule, IonicModule],
-  exports: [InputTextComponent, ButtonComponent, UserResumedDetailsComponent, CarResumedDetailsComponent],
+  exports: [InputTextComponent, ButtonComponent, UserResumedDetailsComponent, CarResumedDetailsComponent, UserCardComponent],
 })
 export class SharedModule {}

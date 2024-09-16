4b4a5e2c8bb8b71a248d3692aec9192c02071dc0
vitorrecoaro <vitordearec@gmail.com>
2024-09-16 13:56:15 -0300

    feat: filling the icons on nav bar when current tab is selected

--- VemComigo-app/src/app/components/bottom-nav-bar/bottom-nav-bar.component.ts
+++ VemComigo-app/src/app/components/bottom-nav-bar/bottom-nav-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
+import { RouterService } from 'src/app/services/router/router.service';
 
 @Component({
   selector: 'app-bottom-nav-bar',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./bottom-nav-bar.component.scss'],
 })
 export class BottomNavBarComponent {
+  @ViewChild('tabs') tabsComponent: IonTabs | null = null;
+
   constructor() {}
+
+  public getIconName(iconName: string, tabName: string) {
+    const currentTab = this.getCurrentTab();
+
+    if (currentTab === tabName) return iconName;
+
+    return `${iconName}-outline`;
+  }
+
+  public getCurrentTab() {
+    if (this.tabsComponent === null) return '';
+
+    return this.tabsComponent.getSelected();
+  }
 }

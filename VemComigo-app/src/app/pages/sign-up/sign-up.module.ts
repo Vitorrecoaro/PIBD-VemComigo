cb54a4b482909540d3fb019250395198e522019a
vitorrecoaro <vitordearec@gmail.com>
2024-09-16 21:51:45 -0300

    feat: hide header directive

--- VemComigo-app/src/app/pages/sign-up/sign-up.module.ts
+++ VemComigo-app/src/app/pages/sign-up/sign-up.module.ts
@@ -8,9 +8,10 @@ import { SignUpPageRoutingModule } from './sign-up-routing.module';
 
 import { SignUpPage } from './sign-up.page';
 import { SharedModule } from '../../shared/shared.module';
+import { SharedDirectivesModule } from 'src/app/directives/shared-directives.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, SignUpPageRoutingModule, SharedModule],
+  imports: [CommonModule, FormsModule, IonicModule, SignUpPageRoutingModule, SharedModule, SharedDirectivesModule],
   declarations: [SignUpPage],
 })
 export class SignUpPageModule {}
